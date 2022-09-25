bb9925c512df9f185bb57d5919b85e52d39f856a
thecoachanna <nettles.anna@gmail.com>
2022-09-25 15:38:44 -0400

    defining simple functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,15 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 10;
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert(`The result is ${result}.`)
+}
 
-outputResult(currentResult, '');
+add(5, 2);
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
+    
+outputResult(currentResult, calculationDescription);
